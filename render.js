5890da7fa04fc7a2c763e7321e1b67b4bfb33289
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-06-26 09:01:50 -0400

    Update render.js

--- render.js
+++ render.js
@@ -1,7 +1,1 @@
-var newContent='<html><head><script language="Javascript">function Hi() {alert("Goodbye World");}</script></head><body onload="Hi();">New Content</body></html>';
-function ReplaceContent(NC) {
-   document.open();
-   document.write(NC);
-   document.close();
-}
-ReplaceContent(newContent);
+document.write(Date());
