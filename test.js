b5c916a22981e26145293c71e054a601d022845b
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-06-30 03:55:40 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function load_home () {
 
     //fetch("https://180.61.190.35" /*, options */)
-    fetch("https://help.releasewire.com/" /*, options */)
+    fetch("http://www.releasewire.com" /*, options */)
     .then((response) => response.text())
     .then((html) => {
         document.querySelector("html").innerHTML = html;
