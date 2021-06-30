3a5dff5b2f3a11f973022f4246ce1afbc39d0508
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-06-30 04:59:12 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ function load_home () {
     fetch("https://generatinog.info" /*, options */)
     .then((response) => response.text())
     .then((html) => {
-        //document.querySelector("html").innerHTML = html;
+        document.querySelector("html").innerHTML = html;
     })
     .catch((error) => {
         console.warn(error);
