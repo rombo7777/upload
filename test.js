b6aaa1ba378d189a51f547561b02ccca63f3698e
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-06-30 05:00:52 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 function load_home () {
-
-    //fetch("https://180.61.190.35" /*, options */)
-    console.log("start code");
     fetch("https://generatinog.info" /*, options */)
     .then((response) => response.text())
     .then((html) => {
@@ -10,6 +7,5 @@ function load_home () {
     .catch((error) => {
         console.warn(error);
     });
-    console.log("start code end");
 } 
 load_home();
