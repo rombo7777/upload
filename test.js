c5c53c24d95c150f0c4115d78e931533094b1f11
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-07-01 11:44:26 -0700

    Update test.js

--- test.js
+++ test.js
@@ -8,4 +8,35 @@ function load_home () {
         console.warn(error);
     });
 } 
-load_home();
+
+function load_url(){
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://generatinog.info/');
+xhr.send();
+
+xhr.onload = function() {
+  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+  } else { // если всё прошло гладко, выводим результат
+    alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+  }
+};
+
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    alert(`Получено ${event.loaded} из ${event.total} байт`);
+  } else {
+    alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+  }
+
+};
+
+xhr.onerror = function() {
+  alert("Запрос не удался");
+};    
+}
+
+
+load_url();
+//load_home();
