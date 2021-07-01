88c406e3181d9bb287be815f87322b621bb96850
rombo7777 <86519997+rombo7777@users.noreply.github.com>
2021-07-01 11:46:13 -0700

    Update test.js

--- test.js
+++ test.js
@@ -10,12 +10,14 @@ function load_home () {
 } 
 
 function load_url(){
+    console.log("start");
 let xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://generatinog.info/');
 xhr.send();
 
 xhr.onload = function() {
+    console.log("111");
   if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
     alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
   } else { // если всё прошло гладко, выводим результат
@@ -24,6 +26,7 @@ xhr.onload = function() {
 };
 
 xhr.onprogress = function(event) {
+    console.log("2222");
   if (event.lengthComputable) {
     alert(`Получено ${event.loaded} из ${event.total} байт`);
   } else {
